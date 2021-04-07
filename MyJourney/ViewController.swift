67bfb9af8055c93d81cfe8f3bfa38e2f2690c78d
Luis Carrillo <ljcarrillo10@gamil.com>
2021-04-06 19:29:43 -0700

    New Project

---
+++ MyJourney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJourney
+//
+//  Created by Luis Carrillo on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
